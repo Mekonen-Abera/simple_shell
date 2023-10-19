33b7a3968fa404c7141b508eb4bc91d3fc7d6e0f
Mekonen-Abera <makoninabera@gmail.com>
2023-10-19 01:50:35 -0700

    C-Simple Shell

--- execute.c
+++ execute.c
@@ -11,31 +11,33 @@ int execute(data_of_program *data)
 	int retval = 0, status;
 	pid_t pidd;
 
+	/* check for program in built ins */
 	retval = builtins_list(data);
-	if (retval != -1)
+	if (retval != -1)/* if program was found in built ins */
 		return (retval);
 
+	/* check for program file system */
 	retval = find_program(data);
 	if (retval)
-	{
+	{/* if program not found */
 		return (retval);
 	}
 	else
-	{
-		pidd = fork();
+	{/* if program was found */
+		pidd = fork(); /* create a child process */
 		if (pidd == -1)
-		{
+		{ /* if the fork call failed */
 			perror(data->command_name);
 			exit(EXIT_FAILURE);
 		}
 		if (pidd == 0)
-		{
+		{/* I am the child process, I execute the program*/
 			retval = execve(data->tokens[0], data->tokens, data->env);
-			if (retval == -1)
+			if (retval == -1) /* if error when execve*/
 				perror(data->command_name), exit(EXIT_FAILURE);
 		}
 		else
-		{
+		{/* I am the father, I wait and check the exit status of the child */
 			wait(&status);
 			if (WIFEXITED(status))
 				errno = WEXITSTATUS(status);
@@ -45,3 +47,4 @@ int execute(data_of_program *data)
 	}
 	return (0);
 }
+
